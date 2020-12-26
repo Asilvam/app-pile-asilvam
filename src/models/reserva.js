32ff8d853923c5f5a100f6e8457f5565672dda7e
alejandro silva <alejandro.silva.m@gmail.com>
2020-12-26 16:23:54 -0300

    mejoras indentacion

--- src/models/reserva.js
+++ src/models/reserva.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const ReservaSchema = new Schema({
-  email: { type: String, required: true,trim: true },
-  nombre: { type: String, required: true,trim: true },
-  fecha: { type: Date, required: true,trim: true },
-  fechaST: { type: String,required: true ,trim: true },
-  hora: { type: String, required: true,trim: true },
-  depto: { type: String, required: true,trim: true },
-  celular: { type: String, required: true,trim: true },
-  numero: { type: Number, required: true,trim: true },
+  email: { type: String, required: true, trim: true },
+  nombre: { type: String, required: true, trim: true },
+  fecha: { type: Date, required: true, trim: true },
+  fechaST: { type: String, required: true, trim: true },
+  hora: { type: String, required: true, trim: true },
+  depto: { type: String, required: true, trim: true },
+  celular: { type: String, required: true, trim: true },
+  numero: { type: Number, required: true, trim: true },
   created_at: { type: Date, default: Date.now() },
 });
 
