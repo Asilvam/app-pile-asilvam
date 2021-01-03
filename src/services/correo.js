3164f2f17f873593d0608e5c34ebdebef5c3a511
alejandro silva <alejandro.silva.m@gmail.com>
2021-01-03 12:14:28 -0300

    mejora texto anulacion reserva

--- src/services/correo.js
+++ src/services/correo.js
@@ -19,47 +19,50 @@ module.exports = {
       <br>
       Antes de hacer ingreso al área de la piscina, debes tomarte la temperatura con un termómetro infrarrojo disponible en conserjería. <br>
       De salir la temperatura sobre lo normal, se te negará la entrada a la piscina, invitando a que te realices un examen de PCR. <br>
-En caso de no hacerlo por cualquier razón, el conserje está facultado para pedirte que abandones la piscina. <strong>¡No vayas a olvidarlo! </strong> <br>
-<br>
-Recuerda que: <br>
-La mantención de la piscina en temporada alta es de 2 veces por semana, por este motivo está cerrada todo el día lunes y la mañana del jueves hasta las 14:00 hrs.<br>
-Para mantener la piscina en funcionamiento necesitamos que tod@s sigan las normas.<br>
-  <br>
-  Protocolo piscina por covid-19:<br>
-  Aforo<br>
-  <li>En el área piscina, se permitirán sólo 4 (cuatro) personas dentro del recinto, quiere decir dentro del perímetro de la reja.<br>
-<li>En el área de los jardines podrán estar hasta un máximo de 10 (diez) personas, con una distancia social de 10 metros.<br>
-  <br>
-  Medidas especiales:<br>
-  <li>Queda prohibido el ingreso de visitas al sector de la piscina.<br>
-  <li>No se puede hacer uso de reposeras, ni de juguetes inflables al interior o exterior de la piscina.<br>
-  <li>Quienes estén con sus trajes de baño, deberán utilizar en todo momento su mascarilla, y solo sacarla cuando ingresen a la piscina.<br>
-  <li>En la entrada de la piscina, mantendremos un depósito de alcohol gel, el cual será de uso obligatorio.<br>
-  <li>La piscina se mantendrá operativa siempre que tod@s cooperen y cumplan con las indicaciones. 
-<br><br>Hay que recordar que la pandemia no ha terminado y debemos continuar con los cuidados recomendados.<br>
-  <br>
-  Horario de funcionamiento:<br>
-  <br>
-  <li>Lunes              - Cerrado todo el día por mantención</li> 
-  <li>Martes            - 10:00 a 20:00</li>
-  <li>Miércoles       - 10:00 a 20:00</li>
-  <li>Jueves             - Cerrado hasta las 14:00 horas por mantención</li>
-  <li>Viernes           - 10:00 a 20:00</li> 
-  <li>Sábado            - 10:00 a 20:00</li> 
-  <li>Domingo         - 10:00 a 20:00</li>
-  
-  <br>
-  Saludos, <br>
-  Comunidad Edificio José Domingo Cañas 1550<br>
-  <br>
-  Nota: Este es un correo de respuesta automática, por lo tanto no contestes ni envíes correos a esta dirección de email.
- <span>`;
+      En caso de no hacerlo por cualquier razón, el conserje está facultado para pedirte que abandones la piscina. <strong>¡No vayas a olvidarlo! </strong> <br>
+      <br>
+      Recuerda que: <br>
+      La mantención de la piscina en temporada alta es de 2 veces por semana, por este motivo está cerrada todo el día lunes y la mañana del jueves hasta las 14:00 hrs.<br>
+      Para mantener la piscina en funcionamiento necesitamos que tod@s sigan las normas.<br>
+      <br>
+      Protocolo piscina por covid-19:<br>
+      Aforo<br>
+      <li>En el área piscina, se permitirán sólo 4 (cuatro) personas dentro del recinto, quiere decir dentro del perímetro de la reja.<br>
+      <li>En el área de los jardines podrán estar hasta un máximo de 10 (diez) personas, con una distancia social de 10 metros.<br>
+      <br>
+      Medidas especiales:<br>
+      <li>Queda prohibido el ingreso de visitas al sector de la piscina.<br>
+      <li>No se puede hacer uso de reposeras, ni de juguetes inflables al interior o exterior de la piscina.<br>
+      <li>Quienes estén con sus trajes de baño, deberán utilizar en todo momento su mascarilla, y solo sacarla cuando ingresen a la piscina.<br>
+      <li>En la entrada de la piscina, mantendremos un depósito de alcohol gel, el cual será de uso obligatorio.<br>
+      <li>La piscina se mantendrá operativa siempre que tod@s cooperen y cumplan con las indicaciones. 
+      <br><br>Hay que recordar que la pandemia no ha terminado y debemos continuar con los cuidados recomendados.<br>
+      <br>
+      Horario de funcionamiento:<br>
+      <br>
+      <li>Lunes              - Cerrado todo el día por mantención</li> 
+      <li>Martes            - 10:00 a 20:00</li>
+      <li>Miércoles       - 10:00 a 20:00</li>
+      <li>Jueves             - Cerrado hasta las 14:00 horas por mantención</li>
+      <li>Viernes           - 10:00 a 20:00</li> 
+      <li>Sábado            - 10:00 a 20:00</li> 
+      <li>Domingo         - 10:00 a 20:00</li>
+      
+      <br>
+      Saludos, <br>
+      Comunidad Edificio José Domingo Cañas 1550<br>
+      <br>
+      Nota: Este es un correo de respuesta automática, por lo tanto no contestes ni envíes correos a esta dirección de email.
+      <span>`;
     let subjectReserva = "Reserva de Piscina ✔";
-    let textoElimina = `<b> Hola  ${req.nombre},<br> Tu Reserva para el dia  ${req.fechaST} <br>
+    let textoElimina = `<span> Hola  ${req.nombre},<br> Tu Reserva para el dia  ${req.fechaST} <br>
       Ha sido Eliminada con Exito.<br>
       <br>
-      Salu2, <br>
-      Equipo "Pile JDC" </b>`;
+      Saludos, <br>
+      Comunidad Edificio José Domingo Cañas 1550<br>
+      <br>
+      Nota: Este es un correo de respuesta automática, por lo tanto no contestes ni envíes correos a esta dirección de email.
+      </span>`;
     let subjectElimna = "Anulacion de Reserva";
     let textoHtml = "";
     let subject = "";
