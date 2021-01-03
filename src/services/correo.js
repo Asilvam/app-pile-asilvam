50a4d2bb73c583f66bd04950b4b131fd71d2b79f
alejandro silva <alejandro.silva.m@gmail.com>
2021-01-03 14:50:39 -0300

    mejoras correo texto

--- src/services/correo.js
+++ src/services/correo.js
@@ -13,7 +13,7 @@ module.exports = {
       },
     });
     // send mail with defined transport object
-    let textoReserva = `<span> Hola  ${req.nombre},<br> <br>Este Correo confirma tu reserva. <br> <br>
+    let textoReserva = `<span> Hola  ${req.nombre},<br> <br>Este Correo confirma tu reserva. <br> 
     Para el dia ${req.fecha}, desde las ${req.hora} horas.<br>
     Es para ${req.numero} personas.<br>
     <br>
@@ -27,14 +27,14 @@ module.exports = {
     <br>
     Protocolo piscina por covid-19:<br>
     Aforo<br>
-    <li>En el área piscina, se permitirán sólo 4 (cuatro) personas dentro del recinto, quiere decir dentro del perímetro de la reja.</li><br>
-    <li>En el área de los jardines podrán estar hasta un máximo de 10 (diez) personas, con una distancia social de 10 metros.</li><br>
+    <li>En el área piscina, se permitirán sólo 4 (cuatro) personas dentro del recinto, quiere decir dentro del perímetro de la reja.</li>
+    <li>En el área de los jardines podrán estar hasta un máximo de 10 (diez) personas, con una distancia social de 10 metros.</li>
     <br>
     Medidas especiales:<br>
-    <li>Queda prohibido el ingreso de visitas al sector de la piscina.</li><br>
-    <li>No se puede hacer uso de reposeras, ni de juguetes inflables al interior o exterior de la piscina.</li><br>
-    <li>Quienes estén con sus trajes de baño, deberán utilizar en todo momento su mascarilla, y solo sacarla cuando ingresen a la piscina.</li><br>
-    <li>En la entrada de la piscina, mantendremos un depósito de alcohol gel, el cual será de uso obligatorio.</li><br>
+    <li>Queda prohibido el ingreso de visitas al sector de la piscina.</li>
+    <li>No se puede hacer uso de reposeras, ni de juguetes inflables al interior o exterior de la piscina.</li>
+    <li>Quienes estén con sus trajes de baño, deberán utilizar en todo momento su mascarilla, y solo sacarla cuando ingresen a la piscina.</li>
+    <li>En la entrada de la piscina, mantendremos un depósito de alcohol gel, el cual será de uso obligatorio.</li>
     <li>La piscina se mantendrá operativa siempre que tod@s cooperen y cumplan con las indicaciones.</li> 
     <br><br>Hay que recordar que la pandemia no ha terminado y debemos continuar con los cuidados recomendados.<br>
     <br>
@@ -54,7 +54,7 @@ module.exports = {
     Nota: Este es un correo de respuesta automática, por lo tanto no contestes ni envíes correos a esta dirección de email.
     <span>`;
     let subjectReserva = "Reserva de Piscina ✔";
-    let textoElimina = `<span> Hola  ${req.nombre},<br> Tu Reserva para el dia  ${req.fechaST} <br>
+    let textoElimina = `<span> Hola  ${req.nombre},<br> <br> Tu Reserva para el dia  ${req.fechaST} <br>
       Ha sido Eliminada con Exito.<br>
       <br>
       Saludos, <br>
